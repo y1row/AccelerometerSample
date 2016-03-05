9f1987ece10e15945a458f4e3c50b8185f2aa3e5
y-kato <y-kato@mapion.co.jp>
2016-03-05 22:38:34 +0900

    移動量の計算

--- app/src/main/java/y1row/android/app/accelerometersample/MainActivity.kt
+++ app/src/main/java/y1row/android/app/accelerometersample/MainActivity.kt
@@ -14,6 +14,8 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
+    val MIN_THRESHOLD = 0.2
+
     val sensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
@@ -25,8 +27,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         findViewById(R.id.line_chart_2) as LineChart;
     }
 
-    val originData = AccelerometerData()
-    val incrementAcceleration = AccelerometerData() // 瞬間的な増分データ
+    val data1 = AccelerometerData()
+    val data2 = AccelerometerData() // 瞬間的な増分データ
 
     var coordinate: Coordinate? = null
     var calcTime: Long = 0L;
@@ -37,15 +39,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         lineChart.run {
             data = LineData()
-            data.addDataSet(originData.dataSetX)
-            data.addDataSet(originData.dataSetY)
-            data.addDataSet(originData.dataSetZ)
+            data.addDataSet(data1.dataSetX)
+            data.addDataSet(data1.dataSetY)
+            data.addDataSet(data1.dataSetZ)
         }
         incrementLineChart.run {
             data = LineData()
-            data.addDataSet(incrementAcceleration.dataSetX)
-            data.addDataSet(incrementAcceleration.dataSetY)
-            data.addDataSet(incrementAcceleration.dataSetZ)
+            data.addDataSet(data2.dataSetX)
+            data.addDataSet(data2.dataSetY)
+            data.addDataSet(data2.dataSetZ)
         }
     }
 
@@ -73,35 +75,46 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         when (p0.sensor?.type) {
             Sensor.TYPE_ACCELEROMETER -> {
                 //Timber.d("onSensorChanged : x[${x}] y[${y}] z[${z}]")
-                originData.run {
-                    addValues(p0.values.toTypedArray())
-
-                    lineX.add(Entry(lowPassX, dataSetX.entryCount))
-                    lineY.add(Entry(lowPassY, dataSetY.entryCount))
-                    lineZ.add(Entry(lowPassZ, dataSetZ.entryCount))
-                }
-                incrementAcceleration.run {
+                data1.run {
                     addValues(p0.values.toTypedArray())
 
                     lineX.add(Entry(rawAX, dataSetX.entryCount))
                     lineY.add(Entry(rawAY, dataSetY.entryCount))
                     lineZ.add(Entry(rawAZ, dataSetZ.entryCount))
                 }
+                if (data1.rawAX > MIN_THRESHOLD
+                        || data1.rawAY > MIN_THRESHOLD
+                        || data1.rawAZ > MIN_THRESHOLD) {
+
+                    val time: Long = System.currentTimeMillis()
+                    val interval: Long = time - calcTime
+                    calcTime = time;
+
+                    if (time == interval) return // 初回なら抜ける
+
+                    coordinate?.let {
+                        it.calc(data2, interval)
+                        Timber.d("coordinate : ${it.toString()}")
+                    }
+                }
+
+                data2.run {
+                    addValues(p0.values.toTypedArray())
+
+                    if (coordinate != null) {
+                        lineX.add(Entry(coordinate?.x!!.toFloat(), dataSetX.entryCount))
+                        lineY.add(Entry(coordinate?.y!!.toFloat(), dataSetY.entryCount))
+                        lineZ.add(Entry(coordinate?.z!!.toFloat(), dataSetZ.entryCount))
+                    } else {
+                        lineX.add(Entry(0f, dataSetX.entryCount))
+                        lineY.add(Entry(0f, dataSetY.entryCount))
+                        lineZ.add(Entry(0f, dataSetZ.entryCount))
+                    }
+                }
+
                 invalidateChart(lineChart)
                 invalidateChart(incrementLineChart)
-
-                val time: Long = System.currentTimeMillis()
-                val interval: Long = time - calcTime
-                calcTime = time;
-
-                if (time == interval) return // 初回なら抜ける
-
-                coordinate?.let {
-                    it.calc(incrementAcceleration, interval)
-                    Timber.d("coordinate : ${it.toString()}")
-                }
             }
-
             Sensor.TYPE_MAGNETIC_FIELD -> {
                 coordinate = Coordinate(p0.values)
             }
